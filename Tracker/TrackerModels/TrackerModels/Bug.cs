161cc6e639ca6f29d3a1fc3b594bc5453a54967a
Hieu <daotranh@uci.edu>
2020-11-23 17:21:08 -0800

    Added a models project. Edited post-build events to copy to common bin folder.

---
+++ Tracker/TrackerModels/TrackerModels/Bug.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerModels
+{
+    public class Bug
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Severity { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
